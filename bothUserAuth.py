c50caa222a49dd17edadce416c69e4826b895f5d
Ivan Khurudzhi <khurudzhi@wisc.edu>
2021-04-17 14:11:54 -0500

    Can add collaborative playlist to library

--- bothUserAuth.py
+++ bothUserAuth.py
@@ -25,35 +25,41 @@ def getPlaylistTracksIDs(user, playlist_id, sp):
         ids.append(track['id'])
     return ids
 
-def authUser():
-    user = input("Enter profile link for user: ")
-    user = user.replace('https://open.spotify.com/user/', '')
-    user = user.split('?')
-    user = user[0]
-    token = util.prompt_for_user_token(
-        username=user,
-        scope='playlist-modify-public',
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri="http://localhost:8889/callback"
-    )
-    if token:
-       sp = spotipy.Spotify(auth=token)
-    return sp, user
-
-def printIDs(user, playlist, sp):
-    ids  = getPlaylistTracksIDs(user, playlist, sp)
-    for i in ids:
-          print(i)
-
-
 client_id = '46c1cfc8247e4267b8f5516f9a61affc'
 client_secret = '704027695cb84a95b86351d933d63acd'
-
-sp1, user1 = authUser()
 myPlaylist = "1MhgN6Lgn83GprBszMayql"
-printIDs(user1, myPlaylist, sp1)
-
-sp2, user2 = authUser()
 ryanPlaylist = "4wn7D16SNoiniwzooOYS2x"
-printIDs(user2, ryanPlaylist, sp2)
+
+user = input("Enter profile link for user: ")
+user = user.replace('https://open.spotify.com/user/', '')
+user = user.split('?')
+user = user[0]
+token = util.prompt_for_user_token(
+    username=user,
+    scope='playlist-modify-public playlist-modify-private playlist-read-collaborative',
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri="http://localhost:8889/callback"
+)
+if token:
+   sp = spotipy.Spotify(auth=token)
+
+user2 = input("Enter profile link for user: ")
+user2 = user2.replace('https://open.spotify.com/user/', '')
+user2 = user2.split('?')
+user2 = user2[0]
+token2 = util.prompt_for_user_token(
+    username=user2,
+    scope='playlist-modify-public playlist-modify-private playlist-read-collaborative',
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri="http://localhost:8889/callback"
+)
+if token2:
+   sp2 = spotipy.Spotify(auth=token2)
+
+ids = getPlaylistTracksIDs(user, myPlaylist, sp)
+
+sp.user_playlist_create(user, name = "The New Shared Playlist", public = False, collaborative = True)
+id = str(sp.user_playlists(user)['items'][0]['uri'])
+sp.user_playlist_add_tracks(user, id, ids)
