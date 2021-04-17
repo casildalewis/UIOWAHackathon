f6aae138fe7adf6a74a395e6235e1efea757716d
marykwok <mkwok4@wisc.edu>
2021-04-17 14:26:25 -0500

    Update spotifyMatch.py

--- spotifyMatch.py
+++ spotifyMatch.py
@@ -15,41 +15,42 @@ from datetime import date
 
 client_id = '46c1cfc8247e4267b8f5516f9a61affc'
 client_secret = '704027695cb84a95b86351d933d63acd'
-def createToken(username):
+def createSp(username):
     token = util.prompt_for_user_token(
     username=username,
     scope='playlist-modify-public playlist-read-private playlist-read-collaborative',
     client_id=client_id,
     client_secret=client_secret,
     redirect_uri="http://localhost:8889/callback")
-    if token: return token
+    if token: return spotipy.Spotify(auth=token)
 
 """
 Ivan's code
 Gets songs from playlist chosen by user user
 returns a List of string of song IDs
 """
-def getPlaylistTracksIDs():
-    username = getUser()
-    sp = spotipy.Spotify(auth=createToken(username))
-    playlist = getPlaylists()
+def getPlaylistTracksIDs(usr, username, playlist):
     tracks = []
-    playlist = sp.user_playlist(username, playlist)
+    playlist = usr.user_playlist(username, playlist)
     for item in playlist['tracks']['items']:
         track = item['track']
         tracks.append((track['id'], track['name']))
     return tracks
 
-def getPlaylists():
-    playlist = input("Enter playlist link you want to match: ")
-    playlist = playlist.replace('https://open.spotify.com/playlist/', '')
-    playlist = playlist.split('?')
-    return playlist[0]
+def getPlaylist(usr, username):
+    playlists = usr.user_playlists(username)
+    for playlist in playlists['items']:
+        print(playlist['name'])
+    chosen = input("Please type the name of playlist to match")
+    for playlist in playlists['items']:
+        if playlist['name'] == chosen: return playlist
+
 
 def getUser():
     user = input("Enter profile link for user: ")
     user = user.replace('https://open.spotify.com/user/', '')
     user = user.split('?')
+    print(user[0])
     return user[0]
 
 def compareList(track_usr1, track_usr2):
@@ -60,9 +61,15 @@ def compareList(track_usr1, track_usr2):
     return match
 
 print("For user 1: ")
-track_usr1 = getPlaylistTracksIDs()
+username1 = getUser()
+usr1 = createSp(username1)
+usr1_playlist = getPlaylist(usr1, username1)['id']
+track_usr1 = getPlaylistTracksIDs(usr1, username1, usr1_playlist)
 print("For user 2: ")
-track_usr2 = getPlaylistTracksIDs()
+username2 = getUser()
+usr2 = createSp(username2)
+usr2_playlist = getPlaylist(usr2, username2)['id']
+track_usr2 = getPlaylistTracksIDs(usr2, username2, usr2_playlist)
 match = compareList(track_usr1, track_usr2)
 for track in match:
     print(track[1])
