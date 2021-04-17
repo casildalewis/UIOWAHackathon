74a71eefe57eac7148d9ed2213407625650b647d
Ivan Khurudzhi <khurudzhi@wisc.edu>
2021-04-17 12:51:44 -0500

    Update bothUserAuth.py

--- bothUserAuth.py
+++ bothUserAuth.py
@@ -25,47 +25,35 @@ def getPlaylistTracksIDs(user, playlist_id, sp):
         ids.append(track['id'])
     return ids
 
+def authUser():
+    user = input("Enter profile link for user: ")
+    user = user.replace('https://open.spotify.com/user/', '')
+    user = user.split('?')
+    user = user[0]
+    token = util.prompt_for_user_token(
+        username=user,
+        scope='playlist-modify-public',
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri="http://localhost:8889/callback"
+    )
+    if token:
+       sp = spotipy.Spotify(auth=token)
+    return sp, user
+
+def printIDs(user, playlist, sp):
+    ids  = getPlaylistTracksIDs(user, playlist, sp)
+    for i in ids:
+          print(i)
+
+
 client_id = '46c1cfc8247e4267b8f5516f9a61affc'
 client_secret = '704027695cb84a95b86351d933d63acd'
 
-user1 = input("Enter profile link for user 1: ")
-user1 = user1.replace('https://open.spotify.com/user/', '')
-user1 = user1.split('?')
-user = user1[0]
-token1 = util.prompt_for_user_token(
-    username=user1,
-    scope='playlist-modify-public',
-    client_id=client_id,
-    client_secret=client_secret,
-    redirect_uri="http://localhost:8889/callback"
-)
-
-if token1:
-   sp1 = spotipy.Spotify(auth=token1)
-
+sp1, user1 = authUser()
 myPlaylist = "1MhgN6Lgn83GprBszMayql"
+printIDs(user1, myPlaylist, sp1)
 
-ids  = getPlaylistTracksIDs(user1, myPlaylist, sp1)
-for i in ids:
-      print(i)
-
-user2 = input("Enter profile link for user 2: ")
-user2 = user2.replace('https://open.spotify.com/user/', '')
-user2 = user2.split('?')
-user = user2[0]
-token2 = util.prompt_for_user_token(
-    username=user2,
-    scope='playlist-modify-public',
-    client_id=client_id,
-    client_secret=client_secret,
-    redirect_uri="http://localhost:8889/callback"
-)
-
-if token2:
-   sp2 = spotipy.Spotify(auth=token2)
-
+sp2, user2 = authUser()
 ryanPlaylist = "4wn7D16SNoiniwzooOYS2x"
-
-ids  = getPlaylistTracksIDs(user2, ryanPlaylist, sp2)
-for i in ids:
-    print(i)
+printIDs(user2, ryanPlaylist, sp2)
